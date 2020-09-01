62693e0a69c1b9b64fd8d8d9b7f7286098cbd2e1
sebastian-kopiczko <sebastian-kopiczko@gmail.com>
2020-09-01 22:27:50 +0100

    Initial

---
+++ src/config/index.js
@@ -0,0 +1,62 @@
+module.exports = {
+
+    author: "@konstantinmuenster",
+    siteTitle: "Web dveloper - Sebastian Kopiczko",
+    siteShortTitle: "kopiczko s.", // Used as logo text in header, footer, and splash screen
+    siteDescription: "I am a passionate learner looking for new challenges and the opportunity to develop my skills in a positive and supportive team. Feel free to contact me, I would be happy to present myself.",
+    siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
+    siteLanguage: "en_US",
+    siteIcon: "src/content/favicon.png", // Relative to gatsby-config file
+
+    splashScreen: false, // Set this to true if you want to use the splash screen
+
+    // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+    // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
+    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantin.muenster",
+    shownArticles: 3,
+    
+    // There are icons available for the following platforms: 
+    // Medium, GitHub, LinkedIn, XING, Behance
+    socialMedia: [
+        {
+            name: "Linkedin",
+            url: "https://www.linkedin.com/in/sebastian-kopiczko/"
+        },
+        {
+            name: "Github",
+            url: "https://github.com/sebastian-kopiczko"
+        },
+    ],
+  
+    navLinks: {
+        menu: [
+            {
+                name: "About Me",
+                url: "/#about",
+            },
+            {
+                name: "Projects",
+                url: "/#projects",
+        },
+        ],
+        button: {
+            name: "Resume",
+            url: "https://sebastian-kopiczko.github.io/resume-react/",
+        }
+    },
+
+    footerLinks: [
+        {
+            name: "Linkedin",
+            url: "https://www.linkedin.com/in/sebastian-kopiczko/"
+        },
+        {
+            name: "Github",
+            url: "https://github.com/sebastian-kopiczko"
+        },
+        {
+            name: "Resume",
+            url: "https://sebastian-kopiczko.github.io/resume-react/"
+        },
+    ]
+}
